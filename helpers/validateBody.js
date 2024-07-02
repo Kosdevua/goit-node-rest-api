f03090d41294ff7c94141d3233c37be3a3df1a27
Kosdevua <konstantin.vodolozhko@gmail.com>
2024-07-02 14:54:07 +0300

    create app

--- helpers/validateBody.js
+++ helpers/validateBody.js
@@ -1,12 +1,13 @@
 import HttpError from "./HttpError.js";
 
 const validateBody = (schema) => {
-  const func = (req, _, next) => {
+  const func = (req, res, next) => {
     const { error } = schema.validate(req.body);
     if (error) {
       next(HttpError(400, error.message));
+    } else {
+      next();
     }
-    next();
   };
 
   return func;
